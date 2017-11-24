3b02f5142fbbd8a3754c08ce4f5dc0a8b30ea882
rrequero <rrequero@gmail.com>
2017-11-24 10:55:27 +0100

    update dataUrl column

--- app/src/models/country4year.model.js
+++ app/src/models/country4year.model.js
@@ -12,7 +12,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.DOUBLE,
             allowNull: true
         },
-        data_url: {
+        dataUrl: {
             type: DataTypes.STRING,
             allowNull: true
         }
